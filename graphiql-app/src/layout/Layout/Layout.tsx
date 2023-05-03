2828466527dc67567f19c1c9a86a1ae6ac079625
Alina <almaksimovic01@gmail.com>
2023-05-03 14:21:17 +0300

    feat: add chakra

--- graphiql-app/src/layout/Layout/Layout.tsx
+++ graphiql-app/src/layout/Layout/Layout.tsx
@@ -1,15 +1,16 @@
 import { FC, PropsWithChildren } from 'react'
+import { Outlet } from 'react-router'
 
 import Header from '../Header'
 import Footer from '../Footer'
 
 import styles from './Layout.module.scss'
 
-const Layout: FC<PropsWithChildren> = ({ children }) => {
+const Layout: FC<PropsWithChildren> = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <div className={styles.content}>{children}</div>
+        <Outlet />
       <Footer />
     </div>
   )
--- graphiql-app/src/pages/WelcomePage/WelcomePage.tsx
+++ graphiql-app/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,5 +1,19 @@
+import { Input } from "@chakra-ui/input"
+
+import styles from './WelcomePage.module.scss'
+import { Button } from "@chakra-ui/button"
+
+
 const WelcomePage = () => {
-  return <div>WelcomePage</div>
+  return (
+    <div>
+      <div>WelcomePage</div>
+      <Input className={styles.input}/>
+      <Button variant='solid' colorScheme='teal'>
+        Нажми
+      </Button>
+    </div>
+  )
 }
 
 export default WelcomePage
