1b8319006023083e730e5d3c5aa99172bf48fd08
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-05-07 23:01:10 +0300

    refactor: add styles in side bar

--- graphiql-app/src/pages/HomePage/components/SideBar/SideBar.tsx
+++ graphiql-app/src/pages/HomePage/components/SideBar/SideBar.tsx
@@ -1,7 +1,8 @@
 import book from '../../../../assets/images/book.png'
 
-import styles from './SideBar.module.scss'
 import { useSideBar } from './hooks'
+
+import styles from './SideBar.module.scss'
 
 const SideBar = () => {
   const { onClick, openDocumentation, queryOptions, loading } = useSideBar()
@@ -17,7 +18,8 @@ const SideBar = () => {
         />
       </div>
       <div className={openDocumentation ? styles.queryOptions : styles.hidden}>
-        Dock
+        <p className={styles.title}>Docs</p>
+        <p className={styles.subtitle}>A GraphQL schema provides a root type for each kind of operation</p>
         {loading && <div>Loading...</div>}
         {queryOptions?.map(({ name }: { name: string }) => (
           <div key={name}>{name}</div>
