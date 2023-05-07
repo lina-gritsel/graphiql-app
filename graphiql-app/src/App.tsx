37713cf6c1476b96c9d0aa64cef332c71e32bdb0
Alina <almaksimovic01@gmail.com>
2023-05-07 23:36:56 +0300

    feat: add react-query

--- graphiql-app/src/App.tsx
+++ graphiql-app/src/App.tsx
@@ -1,16 +1,20 @@
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import router from './router'
 
 import './scss/styles.scss'
 
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
+    <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <RouterProvider router={router} />
       </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
