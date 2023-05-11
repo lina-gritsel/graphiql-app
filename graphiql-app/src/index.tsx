1392f0ea42ba9fcd92413381465cdb9fbf4d5393
Artur Voloshin <nozeilwrk@gmail.com>
2023-05-11 11:42:26 +0300

    refactor: move suspense into app and user auth into layout

--- graphiql-app/src/index.tsx
+++ graphiql-app/src/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import '../src/i18next'
-
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <React.Suspense fallback="loading">
       <App />
-    </React.Suspense>
   </React.StrictMode>,
 )
--- graphiql-app/src/layout/Layout/Layout.tsx
+++ graphiql-app/src/layout/Layout/Layout.tsx
@@ -1,16 +1,28 @@
+import { Outlet } from 'react-router'
 import Header from '../Header'
 import Footer from '../Footer'
-
 import styles from './Layout.module.scss'
-import { Outlet } from 'react-router'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../../firebase'
+import { AuthContext } from '../../constants/context'
+import ProgressBar from '../../components/ProgressBar/ProgressBar'
 
 const Layout = () => {
+  const [user, loading, error] = useAuthState(auth)
+  const isAuthUser = !!user
+
+  if (loading) {
+    return <ProgressBar />
+  }
+
   return (
-    <div className={styles.container}>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <AuthContext.Provider value={isAuthUser}>
+      <div className={styles.container}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </AuthContext.Provider>
   )
 }
 
